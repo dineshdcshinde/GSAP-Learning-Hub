0e7ceabc262dc46a70c0f7304e9c942b25bbd2d5
Dinesh_DC <dineshshinde81563@gmail.com>
2024-12-06 23:52:18 +0530

    Created imgContainer and imgScroller

--- Clone of NiceAtNoon/script.js
+++ Clone of NiceAtNoon/script.js
@@ -1,1 +1,23 @@
-console.log("hi there!");
+gsap.to(".img-container img", {
+  transform: "translateX(-200%)",
+  scrollTrigger: {
+    trigger: ".img-container img",
+    start: "top 50%",
+    end: "top -100%",
+    scrub: 1,
+    pin: true,
+  },
+});
+
+
+gsap.from(".para-container p",{
+    y: 100,
+    opacity: 0,
+    stagger:0.3,
+    scrollTrigger: {
+      trigger: ".para-container",
+      start: "top 80%",
+      end: "top 40%",
+      scrub: 3,
+    },
+})
